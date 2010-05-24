e38d00ab8b734d6212a0c595ee86a958e99ec1c7
unknown <Tommy@.(none)>
2010-05-24 19:37:16 +0200

    Game class input handling

--- game.d
+++ game.d
@@ -1,5 +1,7 @@
 module Game;
 
+import derelict.sdl.sdl;
+
 import std.stdio;
 
 
@@ -10,16 +12,41 @@ unittest
   // TODO: assert that Derelict SDL and GL loaded OK
   
   // assert that game responds to input
-  assert(game.hasInput() == false);
-  game.receiveInput();
-  assert(game.hasInput() == true);
+  assert(game.hasInput(Game.Input.NOTHING), "Input not cleared on freshly created game");
+  
+  SDL_Event upEvent;
+  upEvent.type = SDL_KEYDOWN;
+  upEvent.key.keysym.sym = SDLK_UP;
+  
+  game.receiveInput(upEvent);
+  
+  assert(game.hasInput(Game.Input.UP), "Game didn't respond properly to input");
 
+  SDL_Event downEvent;
+  downEvent.type = SDL_KEYDOWN;
+  downEvent.key.keysym.sym = SDLK_DOWN;
+  
+  game.receiveInput(downEvent);
+  
+  // TODO: do we need possible multiple inputs in an update? if so assert for UP input too here
+  
+  assert(game.hasInput(Game.Input.DOWN), "Game didn't respond properly to input");
+  
   game.clearInput();
-  assert(game.hasInput() == false);
+  assert(game.hasInput(Game.Input.NOTHING), "Game didn't clear input on request");
   
   assert(game.updateCount == 0);
   game.update();
   assert(game.updateCount == 1);
+  
+  
+  SDL_Event quitEvent;
+  quitEvent.type = SDL_QUIT;
+  
+  game.receiveInput(quitEvent);
+  game.update();
+  
+  assert(!game.running, "Game didn't respond properly to quit event");
 }
 
 
@@ -28,25 +55,62 @@ class Game
 public:
   this()
   {
-    m_hasInput = false;
+    clearInput();
     m_updateCount = 0;
+    m_running = true;
   }
-  
-  
-private:
-  void receiveInput()
+
+
+  private:
+  enum Input
   {
-    m_hasInput = true;
+    NOTHING,
+    QUIT,
+    UP, DOWN
+  }  
+ 
+  
+private:  
+  void receiveInput(SDL_Event event)
+  {
+    switch (event.type)
+    {
+      case SDL_QUIT:
+        m_input = Input.QUIT;
+        break;
+        
+      case SDL_KEYDOWN:
+      {
+        switch (event.key.keysym.sym)
+        {
+          case SDLK_DOWN:
+            m_input = Input.DOWN;
+            break;
+            
+          case SDLK_UP:
+            m_input = Input.UP;
+            break;
+            
+          default:
+            break;
+        }
+        break;
+      }
+      
+      default:
+        break;
+    }
+    
   }
   
   void clearInput()
   {
-    m_hasInput = false;
+    m_input = Input.NOTHING;
   }
   
-  bool hasInput()
+  bool hasInput(const Input p_input)
   {
-    return m_hasInput;
+    return m_input == p_input;
   }
   
   int updateCount()
@@ -57,10 +121,20 @@ private:
   void update()
   {
     m_updateCount++;
+    
+    if (hasInput(Input.QUIT))
+      m_running = false;
+  }
+  
+  bool running()
+  {
+    return m_running;
   }
   
   
 private:
-  bool m_hasInput;
+  Input m_input;
   int m_updateCount;
+  
+  bool m_running;
 }
