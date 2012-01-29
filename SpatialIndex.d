c7c0935cb38886975643dfc1199a74d212941d50
OlaOst <olaa81@gmail.com>
2012-01-29 19:37:15 +0100

    Initial SpatialIndex module

---
+++ SpatialIndex.d
@@ -0,0 +1,39 @@
+/*
+ Copyright (c) 2011 Ola Østtveit
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+module SpatialIndex;
+
+import gl3n.linalg;
+
+
+unittest
+{
+  // let's start with a plain z-order index
+  // the even bits together make up the x coord, the odd bits together make up the y coord
+  
+  // given a component consisting of a position and a radius, we need a way to know which indices it is covering
+  
+  assert(interleave(0, 0) == 0);
+  assert(interleave(0, 1) == 1);
+  assert(interleave(1, 0) == 2);
+  assert(interleave(1, 1) == 3);
+}
