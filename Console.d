8d5c1abae8f5d9fca4c4303cff5c703022ec2e44
OlaOst <olaa81@gmail.com>
2012-02-04 16:00:07 +0100

    First commit of console module

---
+++ Console.d
@@ -0,0 +1,35 @@
+/*
+ Copyright (c) 2012 Ola Østtveit
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
+
+module Console;
+
+
+unittest
+{
+  // a console is a command-like tool that can parse and execute debug commands, and display debug information
+  
+  // it needs to show the latest output
+  
+  // it should have an input line that the user can type commands into
+  
+  // it should hide and deactivate when the toggle console button is pressed
+}
